62d5a9b387518f6bc36bd36d390c07677c978eb2
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 14:59:24 +0100

    were getting somewhere

--- text-to-file-app/src/components/AddOneRFC.js
+++ text-to-file-app/src/components/AddOneRFC.js
@@ -4,12 +4,19 @@ function getKey(key){
 }
 function AddOneRFC({setWords}){
     const keyRef = useRef(1);
+    const wordRef = useRef(null);
+    const meaningRef = useRef(null);
+
+    function addWordHandler(e){
+        console.log("triggered");
+    }
     return (
         <>
         <label htmlFor="word">Word</label>
         <input type="text" name="word" id="word"/>
         <label htmlFor="meaning">Meaning</label>
         <input type="text" name="meaning" id="meaning"/>
+        <button onClick={addWordHandler}>Add Word</button>
         </>
     );
 };
