ec788d3c594e84b23af3492d00461005cbe5ed17
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 13:16:53 +0100

    were getting somewhere

--- text-to-file-app/src/App.js
+++ text-to-file-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [words, setWords] = useState([]);
   const [title, setTitle] = useState("");
+  const anchorRef = useRef(null);
 
   function onTitleChange(e){
     setTitle(e.target.value);
@@ -11,6 +12,15 @@ function App() {
 
   function onCreateClick(e){
     alert("file created");
+    var cleanScript = {
+      "title": title,
+      "words": words 
+  };
+
+  var jsonse = JSON.stringify(cleanScript);
+  var blob = new Blob([jsonse], {type: "application/json"});
+  var url  = URL.createObjectURL(blob);
+  anchorRef.current.href = url;
   }
   return (
     <div className="App">
@@ -21,6 +31,7 @@ function App() {
       <textarea id='words' name='words' rows="35" cols="66"></textarea><br/>
       <button>Add words</button><br/>
       <button onClick={onCreateClick}>Create file</button>
+      <a href='#' download="file" ref={anchorRef}>Download</a>
     </div>
   );
 }
