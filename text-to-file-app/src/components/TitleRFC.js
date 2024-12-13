3b2f97fd6c15f2f814a9f4cb5eb81593274f32d3
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 15:36:46 +0100

    were getting somewhere

--- text-to-file-app/src/components/TitleRFC.js
+++ text-to-file-app/src/components/TitleRFC.js
@@ -1,10 +1,12 @@
+import { AddTitle } from "./ui/AddTitle";
+
 function TitleRFC({title, setTitle}){
     function onTitleChange(e){
         setTitle(e.target.value);
     }
     return (
         <>
-        <p>Title: {title}</p>
+        <AddTitle/>
         <label htmlFor="title">Title</label>
         <input type="text" name="title" id="title" onChange={onTitleChange} value={title} />
         </>
---
+++ text-to-file-app/src/components/ui/AddTitle.js
@@ -0,0 +1,9 @@
+function AddTitle(){
+    return (
+        <>
+        <h3>Add Title</h3>
+        </>
+    );
+};
+
+export {AddTitle};
