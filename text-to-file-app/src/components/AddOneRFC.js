3a51c7b7867907763e4d8eecba34dcf877594257
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 15:52:35 +0100

    were getting somehwere

--- text-to-file-app/src/components/AddOneRFC.js
+++ text-to-file-app/src/components/AddOneRFC.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { AddSingle } from "./ui/AddSingle";
+import { FormRow } from "./ui/FormRow";
 function getKey(key){
     return `single-${key}`;
 }
@@ -21,10 +22,16 @@ function AddOneRFC({setWords}){
     return (
         <>
         <AddSingle/>
+        <FormRow>
         <label htmlFor="word">Word</label>
         <input type="text" name="word" id="word" ref={wordRef}/>
+        </FormRow>
+        
+        <FormRow>
         <label htmlFor="meaning">Meaning</label>
         <input type="text" name="meaning" id="meaning" ref={meaningRef}/>
+        </FormRow>
+        
         <button onClick={addWordHandler}>Add Word</button>
         </>
     );
--- text-to-file-app/src/components/TitleRFC.js
+++ text-to-file-app/src/components/TitleRFC.js
@@ -1,5 +1,5 @@
 import { AddTitle } from "./ui/AddTitle";
-
+import { FormRow } from "./ui/FormRow";
 function TitleRFC({title, setTitle}){
     function onTitleChange(e){
         setTitle(e.target.value);
@@ -7,8 +7,10 @@ function TitleRFC({title, setTitle}){
     return (
         <>
         <AddTitle/>
+        <FormRow>
         <label htmlFor="title">Title</label>
         <input type="text" name="title" id="title" onChange={onTitleChange} value={title} />
+        </FormRow>
         </>
     );
 };
