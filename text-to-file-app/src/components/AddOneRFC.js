bd4b0bd86565c9a501e29808d3b8374ebd70bd58
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 14:56:36 +0100

    were getting somewhere

--- text-to-file-app/src/components/AddOneRFC.js
+++ text-to-file-app/src/components/AddOneRFC.js
@@ -1,4 +1,9 @@
+import { useRef } from "react";
+function getKey(key){
+    return `single-${key}`;
+}
 function AddOneRFC({setWords}){
+    const keyRef = useRef(1);
     return (
         <>
         <p>Not implemented</p>
