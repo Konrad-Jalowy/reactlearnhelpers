07facd21f20f8b3f496c3336c9c8cef106182968
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 13:04:26 +0100

    were getting somewhere

--- text-to-file-app/src/App.js
+++ text-to-file-app/src/App.js
@@ -1,10 +1,11 @@
-
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [words, setWords] = useState([]);
   return (
     <div className="App">
-      
+      {words.length}
     </div>
   );
 }
