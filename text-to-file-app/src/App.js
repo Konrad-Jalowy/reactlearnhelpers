16c86a303bcf54fe88fd5a5b529f69ad525df602
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-13 13:12:19 +0100

    were getting somewhere

--- text-to-file-app/src/App.js
+++ text-to-file-app/src/App.js
@@ -13,8 +13,10 @@ function App() {
       <label htmlFor="title">Title</label>
       <input type="text" name="title" id="title" onChange={onTitleChange} value={title} />
       <p>Title: {title}</p>
-      <label htmlFor="words">Paste your words here:</label>
-      <textarea id='words' name='words' rows="35" cols="66"></textarea>
+      <label htmlFor="words">Paste your words here:</label> <br/>
+      <textarea id='words' name='words' rows="35" cols="66"></textarea><br/>
+      <button>Add words</button><br/>
+      <button>Create file</button>
     </div>
   );
 }
