3d94388fbc194a2fcf880101c5fc456937441b08
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 11:49:36 +0100

    were going somewhere

--- show-words/src/components/WordsContainer.js
+++ show-words/src/components/WordsContainer.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function WordsContainer({words, title, wordsFirst}){
+    const [index, setIndex] = useState(0);
+    const [wordShown, setWordShown] = useState(false);
+    const [defShown, setDefShown] = useState(false);
     return (
         <>
         <p>Not implemented (words container)</p>
+        <h1 className="words-title">{title}</h1>
         </>
     );
 };
