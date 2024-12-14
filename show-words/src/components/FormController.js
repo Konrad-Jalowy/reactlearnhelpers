c52466668192826f37b5ea05fce545fd24b0bfcb
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 10:42:24 +0100

    were getting somewhere

--- show-words/src/components/FormController.js
+++ show-words/src/components/FormController.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function FormController(){
+    const [formMode, setFormMode] = useState('notAccepted');
+    const [title, setTitle] = useState('');
+    const [words, setWords] = useState([]);
+
     return (
         <>
         <p>Not implemented(form controller)</p>
