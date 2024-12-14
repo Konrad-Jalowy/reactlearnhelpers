6b06e4fdc325cd41ef545025db6088f587d30ace
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 11:36:58 +0100

    works

--- show-words/src/components/SetupForm.js
+++ show-words/src/components/SetupForm.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { SetupFormContent } from "./SetupFormContent";
+import { useAppDispatch } from "../context/appcontext";
 function SetupForm({title, words}){
+
+    const dispatch = useAppDispatch();
+
     const [_state, setState] = useState({
         shuffle: false, 
         wordsFirst: true,
@@ -10,6 +14,15 @@ function SetupForm({title, words}){
     const handleSubmit = e => {
         e.preventDefault()
         console.log(JSON.stringify(_state))
+        const payload = {
+            title: title,
+            words: words,
+            shuffle: _state.shuffle,
+            wordsFirst: _state.wordsFirst,
+            take: _state.take
+        };
+        
+        dispatch({type: 'prepare', payload: payload});
     }
 
     const handleChange = e => {
