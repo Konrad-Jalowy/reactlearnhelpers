69810452d43bcf08807f199b2258bd61ec20f599
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 12:34:51 +0100

    works

--- show-words/src/components/WordRFC.js
+++ show-words/src/components/WordRFC.js
@@ -3,7 +3,7 @@ const WordRFC = forwardRef(function WordRFC({idx, words, wordsFirst}, ref){
     const wordText = wordsFirst ? words[idx].word : words[idx].meaning
    
     return (
-        <p ref={ref} className="date-rfc ani-fadein">{wordText}</p>
+        <p ref={ref} className="word-rfc ani-fadein">{wordText}</p>
     );
 });
 
--- show-words/src/components/WordsContainer.js
+++ show-words/src/components/WordsContainer.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import IdxRFC from "./IdxRFC";
 import { WordRFC } from "./WordRFC";
+import { DefRFC } from "./DefRFC";
 function WordsContainer({words, title, wordsFirst}){
     const [index, setIndex] = useState(0);
     const [wordShown, setWordShown] = useState(false);
@@ -17,6 +18,7 @@ function WordsContainer({words, title, wordsFirst}){
         <div className="words-container">
         <IdxRFC idx={index} maxWord={words.length}/>
         <WordRFC idx={index} ref={wordRef} key={index+1} words={words} wordsFirst={wordsFirst} />
+        <DefRFC idx={index} ref={defRef} key={index+2} words={words} wordsFirst={wordsFirst} />
         </div>
         </>
     );
