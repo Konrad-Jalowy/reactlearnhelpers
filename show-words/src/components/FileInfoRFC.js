23be35b2b14610180e31b9c9b9ad9c096be737d9
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 10:41:07 +0100

    were getting somewhere

---
+++ show-words/src/components/FileInfoRFC.js
@@ -0,0 +1,11 @@
+function FileInfoRFC({title}){
+    
+    return (
+        <>
+        <p>Not implemented (fileinfo RFC)</p>
+        <p>Title: {title}</p>
+        </>
+    );
+};
+
+export default FileInfoRFC;
