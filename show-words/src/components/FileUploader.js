0e097d99602e6b4531ecb6a30084308588f952d2
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 10:56:40 +0100

    were getting somewhere

--- show-words/src/components/FileUploader.js
+++ show-words/src/components/FileUploader.js
@@ -1,6 +1,12 @@
 import { useState, useRef } from "react";
 import { objIsCorrect } from "../utils/objIsCorrect";
 function FileUploader(){
+
+    const inputFileRef = useRef(null);
+    const wordsRef = useRef(null);
+    
+    const [uploaderMode, setUploaderMode] = useState('fileNull');
+   
     return (
         <>
         <p>Not implemented(file uploader)</p>
