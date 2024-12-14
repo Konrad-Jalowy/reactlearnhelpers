1ad4442c9c53244b183cfc14c9f13a0254f39634
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 12:31:38 +0100

    works

--- show-words/src/components/WordsContainer.js
+++ show-words/src/components/WordsContainer.js
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import IdxRFC from "./IdxRFC";
+import { WordRFC } from "./WordRFC";
 function WordsContainer({words, title, wordsFirst}){
     const [index, setIndex] = useState(0);
     const [wordShown, setWordShown] = useState(false);
     const [defShown, setDefShown] = useState(false);
+
+    const wordRef = useRef(null);
+    const defRef = useRef(null);
+    const firstRender = useRef(true);
+    const callbackRef = useRef(null);
     return (
         <>
         <p>Not implemented (words container)</p>
         <h1 className="words-title">{title}</h1>
         <div className="words-container">
         <IdxRFC idx={index} maxWord={words.length}/>
+        <WordRFC idx={index} ref={wordRef} key={index+1} words={words} wordsFirst={wordsFirst} />
         </div>
         </>
     );
