c80e4c594c05727e108d8f742053863a835a7b6a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 11:41:41 +0100

    were getting somewhere

--- show-words/src/components/WordsContainerController.js
+++ show-words/src/components/WordsContainerController.js
@@ -1,7 +1,15 @@
+import { useApp } from "../context/appcontext";
+import { useRef, useState } from "react";
 function WordsContainerController(){
+
+    const {take, shuffle, title, words, wordsFirst} = useApp();
+    const wordsRef = useRef(null);
+    const [containerKey, setContainerKey] = useState(1);
+    console.log(words);
     return (
         <>
         <p>Not implemented(words container)</p>
+        <p>Take: {take} shuffle: {shuffle.toString()} title: {title} wordsFirst: {wordsFirst.toString()}</p>
         </>
     );
 };
