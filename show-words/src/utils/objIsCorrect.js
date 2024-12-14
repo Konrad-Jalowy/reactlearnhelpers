043629a37b3bf41298c202c25cc81b9f3e84b177
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 10:18:33 +0100

    file validation ready

--- show-words/src/utils/objIsCorrect.js
+++ show-words/src/utils/objIsCorrect.js
@@ -13,3 +13,45 @@ function wordItemCorrect(wordItem){
     }
     return true;
 }
+
+function objIsCorrect(obj){
+    if(!Object.hasOwn(obj, 'title')){
+        console.log("no title")
+        return false;
+       
+    }
+    if(typeof obj.title !== 'string'){
+        console.log("title not string")
+        return false;
+    }
+    if(obj.title.trim() === ""){
+        console.log("title empty");
+        return false;
+    }
+
+    if(!Object.hasOwn(obj, 'words')){
+        console.log("no words");
+        return false;
+    }
+    if(!Array.isArray(obj.words)){
+        console.log("words not an array");
+        return false;
+    }
+    if(obj.words.length === 0){
+        console.log("words are empty");
+        return false;
+    }
+    
+    let allItemsCorrect = true
+    
+    for(const wordItem of obj.words){
+        if(wordItemCorrect(wordItem)){
+            continue;
+        }
+        allItemsCorrect = false;
+        break;
+    }
+    return allItemsCorrect;
+};
+
+export {objIsCorrect};
