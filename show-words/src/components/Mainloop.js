4a59693598e95b958ecb4eea79aa777a76906f33
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-14 10:39:43 +0100

    were getting somewhere

--- show-words/src/components/Mainloop.js
+++ show-words/src/components/Mainloop.js
@@ -1,7 +1,14 @@
+import { FormController } from "./FormController";
+import { WordsContainerController } from "./WordsContainerController";
+import { useAppWithDispatch } from "../context/appcontext";
 function Mainloop(){
+    const [{loopMode}, dispatch] = useAppWithDispatch();
     return (
         <>
         <p>Not implemented(mainloop)</p>
+        <button onClick={() => dispatch({type: 'setLoopMode', payload: 'started'})}>Fake send file</button>
+        {loopMode === 'fileNotSent' && <FormController/>}
+        {loopMode === 'started' && <WordsContainerController/>}
         </>
     );
 };
