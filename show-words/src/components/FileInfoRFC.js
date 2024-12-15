abcc748e737e56dad3ad9d28c24f6e0bbad32840
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-15 12:12:25 +0100

    css

--- show-words/src/components/FileInfoRFC.js
+++ show-words/src/components/FileInfoRFC.js
@@ -2,7 +2,7 @@ function FileInfoRFC({title, words}){
     
     return (
         <>
-        <p>Make sure thats the file and accept:</p>
+        <p className="info-msg">Make sure thats the file and accept:</p>
         <p>Title: {title}</p>
         <p>Words: {words.length}</p>
         </>
