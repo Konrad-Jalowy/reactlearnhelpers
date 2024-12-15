bf2c4e9cba8e233a026f31a2eebaf0166da02240
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-15 14:01:36 +0100

    css

--- show-words/src/components/SetupFormContent.js
+++ show-words/src/components/SetupFormContent.js
@@ -2,15 +2,15 @@ function SetupFormContent({ data, handleChange, max }){
     const content = (
     <>
     
-         <div>
+         <div className="row">
          <input type="checkbox" name="shuffle" checked={data.shuffle} onChange={handleChange}/>
          <label htmlFor="shuffle">Shuffle</label>
          </div>
-        <div>
+        <div className="row">
         <input type="checkbox" name="wordsFirst" checked={data.wordsFirst} onChange={handleChange}/>
         <label htmlFor="wordsFirst">Words First</label>
         </div>
-        <div>
+        <div className="row">
         <label htmlFor="take">Take:</label>
         <input type="number" name="take" min="1" value={data.take} max={max} onChange={handleChange} />
         </div>
